7e8f170f45a3730026ae8d91e817c3125aeadef2
Brian Nelson <brian@>
2018-10-22 16:44:58 -0700

    add add/delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,41 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();  
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub'),
-    new Task('Coffee break and more talking')
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 4),
+    new Task('Coffee break and more talking', 1)
   ];
+
+  selectedTask: null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+
+  deleteToDo(todoIndex) {
+    this.tasks.splice(todoIndex,1);
+  }
+
+  addToDo(todo, priority) {
+    let newTask = new Task(todo, priority);
+    this.tasks.unshift(newTask);
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 4){
+      return "bg-primary";    
+    } else if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
 
---
+++ src/app/models/recipe.model.ts
@@ -0,0 +1,4 @@
+export class Recipe {
+    public cooked: boolean = false;
+    constructor(public title: string, public ingredients: string[], public directions: string) {   }
+  }
